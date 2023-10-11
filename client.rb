b6637603c6df0a8ba383aad39327951e41531229
Paulo Erichsen <paulo.h.erichsen@gmail.com>
2023-10-10 23:03:08 -0600

    fix the require statement

--- client.rb
+++ client.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby -w
 # -*- coding: utf-8 -*-
 require "socket"
-require 'colorize'
+require_relative 'colorize'
 
 ###############################################################################
 # the Client Class
